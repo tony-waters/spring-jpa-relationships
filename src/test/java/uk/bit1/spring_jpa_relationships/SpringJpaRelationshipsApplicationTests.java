e5c4f362045e092107a3e3f4a174032ef58290c6
tony-waters <2tonywaters@gmail.com>
2026-01-05 18:06:07 +0000

    initial commit

---
+++ src/test/java/uk/bit1/spring_jpa_relationships/SpringJpaRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.bit1.spring_jpa_relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
